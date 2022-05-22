fbbc987080ac8fc0a02d9b6ef52bbc8770a42082
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-22 14:53:06 +0800

    getUserGenre unary grpc call done

---
+++ user-service/src/main/java/com/grpcflix/user/service/UserService.java
@@ -0,0 +1,39 @@
+package com.grpcflix.user.service;
+
+import com.grpcflix.user.entity.User;
+import com.grpcflix.user.repository.UserRepository;
+import com.mike.grpcflix.common.Genre;
+import com.mike.grpcflix.user.UserGenreUpdateRequest;
+import com.mike.grpcflix.user.UserResponse;
+import com.mike.grpcflix.user.UserSearchRequest;
+import com.mike.grpcflix.user.UserServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Locale;
+
+@GrpcService
+public class UserService extends UserServiceGrpc.UserServiceImplBase {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void getUserGenre(UserSearchRequest request, StreamObserver<UserResponse> responseObserver) {
+        UserResponse.Builder builder = UserResponse.newBuilder();
+
+        userRepository.findById(request.getLoginId())
+                        .ifPresent(user -> builder.setName(user.getName())
+                                .setLoginId(user.getLogin())
+                                .setGenre(Genre.valueOf(user.getGenre().toUpperCase())));
+
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateUserGenre(UserGenreUpdateRequest request, StreamObserver<UserResponse> responseObserver) {
+        super.updateUserGenre(request, responseObserver);
+    }
+}
