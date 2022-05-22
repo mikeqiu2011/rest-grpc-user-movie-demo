a7d64acf7aafab487f84282c0c57847f702e455d
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-22 16:55:59 +0800

    userMovieService to call gprc services

---
+++ aggregator-service/src/main/java/com/grpcflix/aggregator/service/UserMovieService.java
@@ -0,0 +1,41 @@
+package com.grpcflix.aggregator.service;
+
+import com.grpcflix.aggregator.dto.RecommendedMovie;
+import com.mike.grpcflix.common.Genre;
+import com.mike.grpcflix.movie.MovieSearchRequest;
+import com.mike.grpcflix.movie.MovieSearchResponse;
+import com.mike.grpcflix.movie.MovieServiceGrpc;
+import com.mike.grpcflix.user.UserSearchRequest;
+import com.mike.grpcflix.user.UserServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserMovieService {
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub userStub;
+
+    @GrpcClient("movie-service")
+    private MovieServiceGrpc.MovieServiceBlockingStub movieStub;
+
+    public List<RecommendedMovie> getUserMovieSuggestion(String loginId){
+        Genre genre = userStub.getUserGenre(UserSearchRequest.newBuilder().setLoginId(loginId).build()).getGenre();
+
+        MovieSearchResponse movieSearchResponse = movieStub.getMovies(MovieSearchRequest.newBuilder().setGenre(genre).build());
+
+        List<RecommendedMovie> movies = movieSearchResponse.getMovieList()
+                .stream()
+                .map(movieDto -> new RecommendedMovie(
+                        movieDto.getTitle(),
+                        movieDto.getYear(),
+                        movieDto.getRating()
+                ))
+                .collect(Collectors.toList());
+
+        return movies;
+
+    }
+}
